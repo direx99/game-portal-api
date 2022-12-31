d93501c657abfec107191ab8eb7eafcf945cf1fb
dinith <dinith.dash@gmail.com>
2022-12-31 18:56:35 +0530

    first commit

---
+++ routes/students.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+let Student = require("../models/Student");
+
+router.route("/add").post((req,res)=>{
+    const name = req.body.name;
+   
+    
+    const newStudent = new Student ({
+        name
+    })
+
+    newStudent.save().then(()=>{
+        res.json("student added")
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+
+router.route("/").get((req,res)=>{
+    Student.find().then((students)=>{
+        res.json(students)
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+
+
+
+module.exports = router;
